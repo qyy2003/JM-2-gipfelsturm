e513bdd88f1ea5ac4a42f848bd1dffce67a22b87
Account course_00270 <course_00270@clariden-ln001.cscs.ch>
2026-05-20 17:02:51 +0200

    scripts to run

--- launch.sh
+++ launch.sh
@@ -97,7 +97,7 @@ case $MODEL_SIZE in
         ;;
     8b)
         NUM_LAYERS=32; HIDDEN=4096; FFN=14336; HEADS=32; KV_HEADS=8
-        MBS=4
+        MBS=2
         ;;
     *)
         echo "Unknown model size: $MODEL_SIZE. Choose: 125m, 350m, 760m, 1.5b, 3b, 8b"
